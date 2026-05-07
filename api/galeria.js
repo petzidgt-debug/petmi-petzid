5cb9de6b615ff2f0c994e3a72d1a71c0d3288617
petzidgt-debug <petzid.gt@gmail.com>
2026-05-07 16:14:07 -0600

    galeria.js

--- api/galeria.js
+++ api/galeria.js
@@ -1,3 +1,5 @@
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxI2MMzjuvhi0AgUzFIBZtP-_97AXxnTrve-ETq7DAg-fYBbqOTE5SWFpLDpCRoXV9M/exec';
+
 const SUPABASE_URL = 'https://ilcreewilnkchvozicyp.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsY3JlZXdpbG5rY2h2b3ppY3lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMDU3NTIsImV4cCI6MjA5MzU4MTc1Mn0.X5QoGsMIKU0oWd0q0qvKYxlbb1tZfMvttBxOwL0BCoM';
 
