d0d70d39dfdd5129a6a88c46c74ca803269f5eb5
petzidgt-debug <petzid.gt@gmail.com>
2026-05-08 17:17:44 -0600

    submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxWm-enyhSrS5X3zJQxLouq7fnX-1bfbs9tGcsbdu6W1a1R2J-ZN7Fwf6roOM8h3qNi/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyYcF_40YFYUnABAXXKmmwZBuLePmPHbWjgnyrXJ2tF0tVhilEOtXK4f70GR7P8GJSX/exec';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
